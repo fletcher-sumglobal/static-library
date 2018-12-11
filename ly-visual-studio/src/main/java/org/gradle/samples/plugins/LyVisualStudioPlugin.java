2055817694a2a18447fbcf8134763a8012ad3b5e
Kent Fletcher <kfletcher@sumglobal.com>
2018-12-11 15:50:58 -0600

    getting the internal plugin to fire

--- ly-visual-studio/src/main/java/org/gradle/samples/plugins/LyVisualStudioPlugin.java
+++ ly-visual-studio/src/main/java/org/gradle/samples/plugins/LyVisualStudioPlugin.java
@@ -5,44 +5,50 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
-import org.gradle.api.plugins.AppliedPlugin;
-import org.gradle.ide.visualstudio.VisualStudioRootExtension;
 import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.plugins.AppliedPlugin;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.ide.visualstudio.VisualStudioRootExtension;
+import org.gradle.samples.tasks.LyVisualStudioTask;
+
+import java.io.File;
 
 public class LyVisualStudioPlugin implements Plugin<Project> {
-  @Override
-  public void apply(Project project) {
-    project.getPluginManager().withPlugin("visual-studio",  new Action<AppliedPlugin>() {
-      @Override
-      public void execute(AppliedPlugin appliedPlugin) {
-        Configuration configuration = createElementsConfiguration(project.getConfigurations(), project.getObjects());
+    @Override
+    public void apply(Project project) {
+        project.getPluginManager().withPlugin("visual-studio", new Action<AppliedPlugin>() {
+            @Override
+            public void execute(AppliedPlugin appliedPlugin) {
+                String name = appliedPlugin.getName();
+                VisualStudioRootExtension visualStudioRootExtension = project.getExtensions().getByType(VisualStudioRootExtension.class);
 
-        VisualStudioRootExtension library = project.getExtensions().getByType(VisualStudioRootExtension.class);
-
-        configure();
-      }
-    });
-
-  }
-
-  private static Configuration createElementsConfiguration(ConfigurationContainer configurations, ObjectFactory objectFactory) {
-    return configurations.create("lyVisualStudioConfig", new Action<Configuration>() {
-      @Override
-      public void execute(Configuration configuration) {
-        configurations.all(new Action<Configuration>() {
-          @Override
-          public void execute(Configuration it) {
-
-          }
+                Configuration configuration = createElementsConfiguration(project.getConfigurations(), project.getObjects());
+                TaskProvider<LyVisualStudioTask> lyvsTask = project.getTasks().register("test", LyVisualStudioTask.class);
+                configure();
+            }
         });
-      }
-    });
-  }
+
+    }
 
 
-  public void configure() {
+    private static Configuration createElementsConfiguration(ConfigurationContainer configurations, ObjectFactory objectFactory) {
+        return configurations.create("lyVisualStudioConfig", new Action<Configuration>() {
+            @Override
+            public void execute(Configuration configuration) {
+                configurations.all(new Action<Configuration>() {
+                    @Override
+                    public void execute(Configuration it) {
 
-  }
+                    }
+                });
+            }
+        });
+    }
+
+
+    public void configure() {
+
+    }
 
 
 }
